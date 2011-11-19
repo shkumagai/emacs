69f7bf4e701146b2cadc48bb3675c0fbb2384b17
Shoji KUMAGAI <take.this.2.your.grave@gmail.com>
2011-11-19 17:56:38 +0900

    Add comments for installation.

--- conf/shkumagai-erlang.el
+++ conf/shkumagai-erlang.el
@@ -1,17 +1,34 @@
 ;; -*- mode: emacs-lisp; coding: utf-8-unix; indent-tabs-mode: nil -*-
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; How to seting up on MacOSX
+;;
+;; Install Erlang
+;;    I used the version of Erlang and Erlang-mode for Emacs in MacPorts.
+;;    MacPorts was install Erlang under /opt/local.
+
 ;; Erlang mode
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (add-to-list 'load-path "/opt/local/lib/erlang/lib/tools-2.6.6.5/emacs")
 (setq erlang-root-dir "/opt/local/lib/erlang")
 (add-to-list 'exec-path "/opt/local/lib/erlang/bin")
 (setq erlang-root-dir "/opt/local/lib/erlang")
 (require 'erlang-start)
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Install Distel
+;;    I used SVN trunk to install the Distel package.
+;;    First of all, you'll need to check it out from the repository using the
+;;    following command:
+;;
+;;    $ svn checkout http://distel.googlecode.com/svn/trunk/ distel
+;;
+;;    And then, you'll need to follow INSTALL instruction to build and update
+;;    .emacs file.
+
+;; For more informations, refer to these blogs.
+;;
+;; http://parijatmishra.wordpress.com/2008/08/15/up-and-running-with-emacs-erlang-and-distel/
+;; http://bc.tech.coop/blog/070528.html
+
 ;; distel
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (add-to-list 'load-path "/usr/local/share/distel/elisp")
 (require 'distel)
 (distel-setup)
--- conf/shkumagai-yasnippet.el
+++ conf/shkumagai-yasnippet.el
@@ -1,5 +1,14 @@
 ;; -*- mode: emacs-lisp; coding: utf-8-unix; indent-tabs-mode: nil -*-
 
+;; How to seting up on MacOSX
+
+;; Install YASnippet package in your own environment according to installation
+;; direction in the document.
+;;
+;; Project site: http://capitaomorte.github.com/yasnippet/
+
+;; And then, add elisp snippet as below to your .emacs or init.el.
+
 ;; Yet another snippet
 (require 'yasnippet)
 (yas/initialize)
