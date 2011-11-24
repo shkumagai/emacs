8164f998c2645223554a16eb3488ee200b1e2d6c
Shoji KUMAGAI <take.this.2.your.grave@gmail.com>
2011-11-24 09:02:32 +0900

    Change all methods of font settings.

--- conf/shkumagai-fonts.el
+++ conf/shkumagai-fonts.el
@@ -29,18 +29,40 @@
 
 ;; Windows
 (when (eq window-system 'w32)
-  (set-face-attribute 'default nil
-          :family "Consolas"
-          :height 110)
-  (set-fontset-font nil 'japanese-jisx0208 (font-spec :family "Meiryo")))
+  (let* ((size 11)
+         (asciifont "Consolas")
+         (jpfont "Meiryo")
+         (h (* size 10))
+         (fontspec (font-spec :family asciifont))
+         (jp-fontspec (font-spec :family jpfont)))
+    (set-face-attribute 'default nil :family asciifont :height h)
+    (set-fontset-font nil 'japanese-jisx0213.2004-1 jp-fontspec)
+    (set-fontset-font nil 'japanese-jisx0213-2 jp-fontspec)
+    (set-fontset-font nil 'katakana-jisx0201 jp-fontspec) ; 半角カナ
+    (set-fontset-font nil '(#x0080 . #x024F) fontspec) ; 分音符付きラテン 
+    (set-fontset-font nil '(#x0370 . #x03FF) fontspec) ; ギリシャ文字
+    ))
 
 ;; Linux
 (when (eq window-system 'x)
-  (set-face-attribute 'default nil
-         :family "Liberation Mono"
-         :height 100)
-  (set-fontset-font nil 'japanese-jisx0208 (font-spec :family "Takao Gothic"))
-  (setq face-font-rescale-alist
- '((".*Liberation Mono.*" . 1.0)
-   (".*Takao Gothic.*" . 1.2)
-   )))
+  (let* ((size 10)
+         (asciifont "Liberation Mono")
+         (jpfont "Takao Gothic")
+         (h (* size 10))
+         (fontspec (font-spec :family asciifont))
+         (jp-fontspec (font-spec :family jpfont)))
+    (set-face-attribute 'default nil :family asciifont :height 100)
+    (set-fontset-font nil 'japanese-jisx0213.2004-1 jp-fontspec)
+    (set-fontset-font nil 'japanese-jisx0213-2 jp-fontspec)
+    (set-fontset-font nil 'katakana-jisx0201 jp-fontspec) ; 半角カナ
+    (set-fontset-font nil '(#x0080 . #x024F) fontspec) ; 分音符付きラテン 
+    (set-fontset-font nil '(#x0370 . #x03FF) fontspec) ; ギリシャ文字
+    )
+
+  ;; define aspect ratio
+  (dolist (elt '((".*Liberation Mono.*" . 1.0)
+                 (".*Takao Gothic.*" . 1.2)
+                 ))
+    (add-to-list 'face-font-rescale-alist elt)
+    ))
+
