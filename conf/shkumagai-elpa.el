a071514869e748c05381798f459c1ecf5377767b
Shoji KUMAGAI <take.this.2.your.grave@gmail.com>
2011-11-25 04:13:47 +0900

    Adjust lines to 80 columns.

--- conf/shkumagai-elpa.el
+++ conf/shkumagai-elpa.el
@@ -1,5 +1,6 @@
 ;; elap (package.el)
 (when (require 'package nil t)
-  (add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
+  (add-to-list 'package-archives
+               '("marmalade" . "http://marmalade-repo.org/packages/"))
   (setq package-user-dir (concat user-emacs-directory "vendor/elpa"))
   (package-initialize))
