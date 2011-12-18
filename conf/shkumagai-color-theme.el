8cbb3d00d30c1c6619f4bdbcdcf9a094c7780603
Shoji KUMAGAI <take.this.2.your.grave@gmail.com>
2011-12-18 23:17:07 +0900

    Add color-theme-solarized.
    - Add new theme elisp of 'color-theme-solarized'.
    - Add setting instruction comment.

--- conf/shkumagai-color-theme.el
+++ conf/shkumagai-color-theme.el
@@ -1,8 +1,16 @@
 ;; -*- mode: emacs-lisp; coding: utf-8-unix; indent-tabs-mode: nil -*-
 
+;; How to setting up on MacOSX
+;;
+;; Install color-theme-solarized
+;;    I use solarized color-theme thrugh color-theme.el. If you want to
+;;    know how to install, see https://github.com/altercation/solarized
+
 ;; Color Theme
 (require 'color-theme)
 (eval-after-load "color-theme"
   '(progn
      (color-theme-initialize)
-     (color-theme-arjen)))
+     ;; (color-theme-arjen)
+     (color-theme-solarized-dark)
+     ))
