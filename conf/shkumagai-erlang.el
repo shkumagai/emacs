a789c46e64e829e4560481850458870acf0f6018
Shoji KUMAGAI <take.this.2.your.grave@gmail.com>
2011-12-18 23:00:30 +0900

    Change erlang environment.
    - Replace erlang executable from using MacPorts to built from sources.
    - Add install instruction of erlang executable
    - Change path to elisp for erlang
    - Adjust codes and comments to 80 columns.

--- conf/shkumagai-erlang.el
+++ conf/shkumagai-erlang.el
@@ -3,13 +3,29 @@
 ;; How to seting up on MacOSX
 ;;
 ;; Install Erlang
-;;    I used the version of Erlang and Erlang-mode for Emacs in MacPorts.
-;;    MacPorts was install Erlang under /opt/local.
+;;    I use Erlang executable which built from source.
+;;    Installation steps in case of R15B is here.
+;;
+;;    1. Download source archive from erlang.org.
+;;
+;;       $ curl -O http://www.erlang.org/download/otp_src_R15B.tar.gz
+;;
+;;    2. Untar the archive, then change directory into there.
+;;
+;;       $ cd otp_src_R15B
+;;
+;;    3. Follow the CMMI (configure, make, make install) steps,
+;;       you should install into your system :)
+;;
+;;       $ ./configure --prefix=/opt/local/erlang/R15B <OTHER_OPTIONS>
+;;       $ make
+;;       $ sudo make install
 
 ;; Erlang mode
-(add-to-list 'load-path "/opt/local/lib/erlang/lib/tools-2.6.6.5/emacs")
-(setq erlang-root-dir "/opt/local/lib/erlang")
-(add-to-list 'exec-path "/opt/local/lib/erlang/bin")
+(add-to-list 'load-path
+             "/opt/local/erlang/R15B/lib/erlang/lib/tools-2.6.6.6/emacs")
+(setq erlang-root-dir "/opt/local/erlang/R15B")
+(add-to-list 'exec-path "/opt/local/erlang/R15B/bin")
 (require 'erlang-start)
 
 ;; Install Distel
@@ -17,15 +33,14 @@
 ;;    First of all, you'll need to check it out from the repository using the
 ;;    following command:
 ;;
-;;    $ svn checkout http://distel.googlecode.com/svn/trunk/ distel
+;;    $ git clone git://github.com/massemanet/distel.git
 ;;
 ;;    And then, you'll need to follow INSTALL instruction to build and update
 ;;    .emacs file.
 
 ;; For more informations, refer to these blogs.
 ;;
-;; http://parijatmishra.wordpress.com/2008/08/15/up-and-running-with-emacs-erlang-and-distel/
-;; http://bc.tech.coop/blog/070528.html
+;; http://
 
 ;; distel
 (add-to-list 'load-path "/usr/local/share/distel/elisp")
@@ -34,24 +49,39 @@
 
 (add-hook 'erlang-mode-hook
           (lambda ()
-            ;; when starting an Erlang shell in Emacs, default in the node name
+            ;; when starting an Erlang shell in Emacs,
+            ;; default in the node name
             (setq inferior-erlang-machine-options '("-sname" "emacs"))
             ;; add Erlang functions to an imenu menu
-            (imenu-add-to-menubar "imenu")))
+            (imenu-add-to-menubar "imenu")
+            ))
+
+;; tel distel to default to that node
+(setq erl-nodename-cache
+      (make-symbol
+       (concat
+        "emacs@"
+        ;; Mac OS X uses "name.local" instead of "name",
+        ;; this should work pretty much anywhere without having
+        ;; to muck with NetInfo
+        ;; ... but I only it on Mac OS X.
+        (car (split-string
+              (shell-command-to-string "hostname"))))))
 
 ;; In additional
-;; ;; A number of the erlang-extended-mode key bindings are useful in the shell too
-;; (defconst distel-shell-keys
-;;   '(("\C-\M-i"   erl-complete)
-;;     ("\M-?"      erl-complete)	
-;;     ("\M-."      erl-find-source-under-point)
-;;     ("\M-,"      erl-find-source-unwind) 
-;;     ("\M-*"      erl-find-source-unwind) 
-;;     )
-;;   "Additional keys to bind when in Erlang shell.")
+;; ;; A number of the erlang-extended-mode key bindings are useful
+;; ;; in the shell too
+(defconst distel-shell-keys
+  '(("\C-\M-i"   erl-complete)
+    ("\M-?"      erl-complete)
+    ("\M-."      erl-find-source-under-point)
+    ("\M-,"      erl-find-source-unwind)
+    ("\M-*"      erl-find-source-unwind)
+    )
+  "Additional keys to bind when in Erlang shell.")
 
-;; (add-hook 'erlang-shell-mode-hook
-;; 	  (lambda ()
-;; 	    ;; add some Distel bindings to the Erlang shell
-;; 	    (dolist (spec distel-shell-keys)
-;; 	      (define-key erlang-shell-mode-map (car spec) (cadr spec)))))
+(add-hook 'erlang-shell-mode-hook
+	  (lambda ()
+	    ;; add some Distel bindings to the Erlang shell
+	    (dolist (spec distel-shell-keys)
+	      (define-key erlang-shell-mode-map (car spec) (cadr spec)))))
