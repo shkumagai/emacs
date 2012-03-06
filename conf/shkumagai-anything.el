62af79c417e8a45c8f5d75c8fe808bbe341f5e04
Shoji KUMAGAI <kumagai@accense.com>
2012-03-06 14:17:22 +0900

    Add anything-etags+.el settings.

--- conf/shkumagai-anything.el
+++ conf/shkumagai-anything.el
@@ -1,2 +1,10 @@
 ;; Anything
 (require 'anything-startup)
+
+;; Anything etags+
+(require 'anything-etags+)
+(setq anything-etags+-use-short-file-name nil)
+(setq anything-etaga+-highlight-tag-after-jump t)
+(global-set-key (kbd "M-.") 'anything-etags+-select-one-key)
+(global-set-key (kbd "M-*") 'anything-etags+-history)
+(setq tags-table-list '("~/bin/TAGS"))
