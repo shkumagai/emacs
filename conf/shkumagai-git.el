118e4d77faaca65f58c36101e6017926a15972ee
Shoji KUMAGAI <take.this.2.your.grave@gmail.com>
2012-03-18 02:56:50 +0900

    Update shkumagai-git.el.

--- conf/shkumagai-git.el
+++ conf/shkumagai-git.el
@@ -8,5 +8,24 @@
 ;;     $ cd path/to/your/elisp/dir
 ;;     $ git clone http://github.com/mhayashi1120/git-emacs.git
 
-(add-to-list 'load-path (concat user-emacs-directory "elisp/git-emacs"))
-(when (require 'git-emacs nil t))
+;; (add-to-list 'load-path (concat user-emacs-directory "elisp/git-emacs"))
+;; (when (require 'git-emacs nil t))
+
+;; How to setting up magit on MacOSX
+;;
+;;  1. Clone from github.
+;;
+;;     $ cd path/to/work/dir
+;;     $ git clone https://github.com/magit/magit.git
+;;
+;;  2. Run make & make install
+;;
+;;     $ make
+;;     $ sudo make install
+;;
+;;  Note:
+;;    libraries will be installed into /usr/local as default.
+;;    If you want to install libraries into where you specified,
+;;    you need to pass --prefix option to the make command.
+
+(when (require 'magit nil t))
