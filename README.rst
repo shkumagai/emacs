8824a72727b8a5a133f890f2565edac495ab585e
Shoji KUMAGAI <take.this.2.your.grave@gmail.com>
2013-04-30 00:17:03 +0900

    Update contents
    - README.txt
    - setup.sh

--- README.rst
+++ README.rst
@@ -10,18 +10,27 @@ setup
 
 1. git clone to local ::
 
-    $ cd <somewhere>
-    $ git clone git@github.com:shkumagai/emacs
+     $ cd <somewhere>
+     $ git clone git@github.com:shkumagai/emacs
 
 2. move to 'emacs' directory, then run setup.sh ::
 
-    $ cd emacs
-    $ bash setup.sh
+     $ cd emacs
+     $ bash setup.sh
 
 
 .. note::
    ``setup.sh`` will remove current ``~/.emacs.d`` director, if exists.
    So, if you won't to do it, you should move it to other place.
+
+3. Run emacs, and then you exec command as below ::
+
+     M-x package-list-packcages
+
+   All packages that needed marked ``I`` for install.
+   Then, hit ``X`` key to execute installation.
+
+   Required package names are listed in elpa/.packages file.
 
 
 byte-compile elisp
--- setup.sh
+++ setup.sh
@@ -1,31 +1,7 @@
 #!/bin/bash
 
-# ## If already exists `~/.emacs.d' directory, remove it.
-# [[ -d "$HOME/.emacs.d" ]] && rm -rf "$HOME/.emacs.d"
+## If already exists `~/.emacs.d' directory, remove it.
+[[ -d "$HOME/.emacs.d" ]] && rm -rf "$HOME/.emacs.d"
 
-# ## make symlink to source on user home.
-# ln -Fis "$PWD" "$HOME/.emacs.d"
-
-if [ ! -z $(which hg) ]; then
-    SED_TARGET="s/^;; \\((load-library \\\"shkumagai-hg\\\")\\)/\\1/g"
-fi
-
-# ret=$(which git)
-# if [ ! -z $ret ]; then
-#     echo $ret
-#     SED_TARGET=";s/^;; \\((load-library \\\"shkumagai-git\\\")\\)/\\1/g"
-# fi
-
-# ret=$(which erl)
-# if [ ! -z $ret ]; then
-#     echo $ret
-#     SED_TARGET=";s/^;; \\((load-library \\\"shkumagai-erl\\\")\\)/\\1/g"
-# fi
-
-# ret=$(which ctags)
-# if [ ! -z $ret ]; then
-#     echo $ret
-#     SED_TARGET=";s/^;; \\((load-library \\\"shkumagai-ctags\\\")\\)/\\1/g"
-# fi
-
-sed -e "$SED_TARGET" init.el
+## make symlink to source on user home.
+ln -Fis "$PWD" "$HOME/.emacs.d"
