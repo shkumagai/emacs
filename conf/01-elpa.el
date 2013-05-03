e706338ec61eb1056d127278e0b9797f57baffe5
Shoji KUMAGAI <take.this.2.your.grave@gmail.com>
2013-05-04 02:51:36 +0900

    Add automatic install function for package.el

--- conf/01-elpa.el
+++ conf/01-elpa.el
@@ -3,4 +3,34 @@
 (when (require 'package nil t)
   (add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
   (add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
+  (if (file-directory-p (concat user-emacs-directory "elpa/archives"))
+      nil
+    (package-list-packages))
   (package-initialize))
+
+;; install packages automatically by package.el
+;; refer to: http://blog.64p.org/entry/2013/05/01/233306
+(mapc
+ (lambda (package)
+   (or (package-installed-p package)
+       (package-install package)))
+ '(auto-complete
+   ctags
+   gist
+   magit
+   undo-tree
+   yasnippet
+   ))
+
+;; delete buffer if it opened
+(dolist (buf '("*Packages*" "*Compile-Log*"))
+  (cond ((get-buffer buf)
+         (kill-buffer buf))))
+
+;; private function
+(defun installed-dir (package)
+  "Return directory path where package with specified name is installed."
+  (let ((dname
+        (car (loop for p in (directory-files package-user-dir)
+                   when (string-match (concat package "-*") p) collect p))))
+  (concat package-user-dir "/" dname)))
