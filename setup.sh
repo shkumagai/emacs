b5fc71eedbbeb1c5001645e16ebb34d3a471782c
Shoji KUMAGAI <take.this.2.your.grave@gmail.com>
2013-05-05 20:41:19 +0900

    Add automatic configuration function into setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,39 @@
 #!/bin/bash
 
+### local functions
+
+function is_exists ()
+{
+    res=$(which ${1})
+    if [[ "${res}" = "" ]]; then
+        echo 0
+    else
+        echo 1
+    fi
+}
+
+function comment_out ()
+{
+    conf=${1}
+    sed -i.orig -e "s/\(^.* \".*-${1}\".*$\)/;; \1/g" init.el
+}
+
+function auto_config ()
+{
+    name=${1}
+    path=${2}
+    if [[ 0 -eq $(is_exists "${path}") ]]; then
+        comment_out ${1}
+    fi
+}
+
+### main
+
+## automatic configuration for init.el
+auto_config migemo "/usr/local/bin/cmigemo"
+auto_config ctags "/opt/local/bin/ctags"
+auto_config go "/opt/local/bin/go"
+
 ## If already exists `~/.emacs.d' directory, remove it.
 [[ -d "$HOME/.emacs.d" ]] && rm -rf "$HOME/.emacs.d"
 
