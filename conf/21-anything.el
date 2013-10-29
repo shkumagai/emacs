790b0a7389c40fb7d9936bf8e15ee75194bced59
Shoji KUMAGAI <kumagai@accense.com>
2013-10-29 16:52:53 +0900

    fix problem which set-mark-command not work

--- conf/21-anything.el
+++ conf/21-anything.el
@@ -6,4 +6,4 @@
 ;; anything-exuberant-ctags
 (when (require 'anything nil t)
   (require 'anything-exuberant-ctags)
-  (define-key global-map (kbd "C-@") 'anything-exuberant-ctags-select-from-here))
+  (define-key global-map (kbd "C-M-@") 'anything-exuberant-ctags-select-from-here))
