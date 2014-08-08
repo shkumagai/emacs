95939130ec59a10b32c6105ee45e1d95b77d981b
Shoji KUMAGAI <take.this.2.your.grave@gmail.com>
2014-08-08 14:21:35 +0900

    add slime setting

---
+++ conf/29-slime.el
@@ -0,0 +1,4 @@
+;; Setup load-path, autoloads and your lisp system.
+;; (add-to-list 'load-path "~/.emacs.d/elisp/slime")
+(require 'slime-autoloads)
+(setq inferior-lisp-program "/opt/local/bin/sbcl")
