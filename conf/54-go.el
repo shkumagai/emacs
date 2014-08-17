daef620c2c6c0feea6c03d8e375b1c1c2c0df69f
Shoji KUMAGAI <take.this.2.your.grave@gmail.com>
2014-08-17 18:27:55 +0900

    add OS dependen path selection

--- conf/54-go.el
+++ conf/54-go.el
@@ -1,6 +1,12 @@
 ;; -*- mode: emacs-lisp; coding: utf-8-unix; indent-tabs-mode: nil -*-
 
 ;; Go
-(add-to-list 'load-path "/usr/share/emacs/site-lisp/go" t)
+(when (eq window-system 'ns)
+  (add-to-list 'load-path "/opt/local/share/emacs/site-lisp" t))
+
+(when (eq window-system 'x)
+  (add-to-list 'load-path "/usr/share/emacs/site-lisp/go" t))
+
+
 (when (require 'go-mode-load nil t)
   (add-hook 'before-save-hook 'gofmt-before-save))
