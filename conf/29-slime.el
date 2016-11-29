614d138aa36da6263e470b60858520372f6970e5
shkumagai <take.this.2.your.grave@gmail.com>
2016-11-29 13:52:56 +0900

    update slime settings

--- conf/29-slime.el
+++ conf/29-slime.el
@@ -3,4 +3,6 @@
 ;; (add-to-list 'load-path "~/.emacs.d/elisp/slime")
 (when (require 'slime-autoloads nil t)
   (setq inferior-lisp-program "sbcl")
-  (slime-setup '(slime-repl)))
+  (setq slime-net-coding-system 'utf-8-unix)
+  (slime-setup '(slime-repl slime-fancy slime-banner slime-indentation slime-company))
+  )
