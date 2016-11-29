e78401bf382e2100f68273d6b7eb88daccc51adc
shkumagai <take.this.2.your.grave@gmail.com>
2016-11-29 13:53:22 +0900

    add environment difference support for exec-path

--- conf/06-exec-path.el
+++ conf/06-exec-path.el
@@ -1,7 +1,9 @@
 ;; -*- mode: emacs-lisp; coding: utf-8-unix; indent-tabs-mode: nil -*-
 
 ;; Exec Paths
-(when (require 'exec-path-from-shell nil t)
-  (exec-path-from-shell-initialize))
-  ;; (let ((envs '("PATH" "GOPATH")))
-  ;;   (exec-path-from-shell-copy-envs envs)))
+(unless (eq window-system 'w32)
+  (when (require 'exec-path-from-shell nil t)
+    (exec-path-from-shell-initialize))
+    ;; (let ((envs '("PATH" "GOPATH")))
+    ;;   (exec-path-from-shell-copy-envs envs)))
+  )
