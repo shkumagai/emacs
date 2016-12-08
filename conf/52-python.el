8eb9f0609d0b38ff6ac9fbec959d305a65333585
shkumagai <take.this.2.your.grave@gmail.com>
2016-12-08 18:02:16 +0900

    update initializing code for company-jedi.el

--- conf/52-python.el
+++ conf/52-python.el
@@ -10,5 +10,6 @@
   :config
   (setq jedi:complete-on-dot t)
   (setq jedi:use-shortcuts t)
-  (add-to-list 'company-backends 'company-jedi)
-  (add-hook 'python-mode-hook 'jedi:setup))
+  (defun my/python-mode-hook ()
+    (add-to-list 'company-backends 'company-jedi))
+  (add-hook 'python-mode-hook 'my/python-mode-hook))
