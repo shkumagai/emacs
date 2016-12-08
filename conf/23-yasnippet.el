e34a015be4b698f94604048a4f42527abb218079
shkumagai <take.this.2.your.grave@gmail.com>
2016-12-08 10:30:10 +0900

    re-add yasnippet

--- conf/23-yasnippet.el
+++ conf/23-yasnippet.el
@@ -12,44 +12,45 @@
 ;; Currently, yasnippets.el is able to install with package.el.
 
 ;; Yet another snippet
-(require 'yasnippet)
-(setq yas-installed-dir (installed-dir "yasnippet"))
-(setq yas-snippet-dirs
-      (let (dirs)
-        (dolist (d '(yas-installed-dir))
-          (setq dirs (cons (concat (symbol-value d) "/snippets") dirs)))
-        dirs)
-      )
-(yas-global-mode 1)
+(when (require 'yasnippet nil t)
+  (setq yas-installed-dir (installed-dir "yasnippet"))
+  (setq yas-snippet-dirs
+        (let (dirs)
+          (dolist (d '(yas-installed-dir))
+            (setq dirs (cons (concat (symbol-value d) "/snippets") dirs)))
+          dirs)
+        )
+  (yas-global-mode 1)
 
-;; Yet another snippet configuration
-;; refer to: http://fukuyama.co/yasnippet
+  ;; Yet another snippet configuration
+  ;; refer to: http://fukuyama.co/yasnippet
 
-;; Insert default snippet
-(define-key yas-minor-mode-map (kbd "C-x i i") 'yas-insert-snippet)
+  ;; Insert default snippet
+  (define-key yas-minor-mode-map (kbd "C-x i i") 'yas-insert-snippet)
 
-;; Create buffer to edit new snippet
-(define-key yas-minor-mode-map (kbd "C-x i n") 'yas-new-snippet)
+  ;; Create buffer to edit new snippet
+  (define-key yas-minor-mode-map (kbd "C-x i n") 'yas-new-snippet)
 
-;; View & Edit all snippets
-(define-key yas-minor-mode-map (kbd "C-x i v") 'yas-visit-snippet-file)
+  ;; View & Edit all snippets
+  (define-key yas-minor-mode-map (kbd "C-x i v") 'yas-visit-snippet-file)
 
-;; Yet another snippet anything interface
-;; refer to: https://github.com/sugyan/dotfiles/blob/master/.emacs.d/conf/04-yasnippet.el
-(eval-after-load "anything-config"
-  '(progn
-     (defun my-yas/prompt (prompt choices &optional display-fn)
-       (let* ((names (loop for choice in choices
-                           collect (or (and display-fn (funcall display-fn choice))
-                                       choice)))
-              (selected (anything-other-buffer
-                         `(((name . ,(format "%s" prompt))
-                            (candidates . names)
-                            (action . (("Insert snippet" . (lambda (arg) arg))))))
-                         "*anything yas/prompt*")))
-         (if selected
-             (let ((n (position selected names :test 'equal)))
-               (nth n choices))
-           (signal 'quit "user quit!"))))
-     (custom-set-variables '(yas/prompt-functions '(my-yas/prompt)))
-     (define-key anything-command-map (kbd "y") 'yas/insert-snippet)))
+  ;; Yet another snippet anything interface
+  ;; refer to: https://github.com/sugyan/dotfiles/blob/master/.emacs.d/conf/04-yasnippet.el
+  (eval-after-load "anything-config"
+    '(progn
+       (defun my-yas/prompt (prompt choices &optional display-fn)
+         (let* ((names (loop for choice in choices
+                             collect (or (and display-fn (funcall display-fn choice))
+                                         choice)))
+                (selected (anything-other-buffer
+                           `(((name . ,(format "%s" prompt))
+                              (candidates . names)
+                              (action . (("Insert snippet" . (lambda (arg) arg))))))
+                           "*anything yas/prompt*")))
+           (if selected
+               (let ((n (position selected names :test 'equal)))
+                 (nth n choices))
+             (signal 'quit "user quit!"))))
+       (custom-set-variables '(yas/prompt-functions '(my-yas/prompt)))
+       (define-key anything-command-map (kbd "y") 'yas/insert-snippet)))
+  )
