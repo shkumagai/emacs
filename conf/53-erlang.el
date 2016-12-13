13ea12e57009fe63752ab21baccb38c99b1ef9de
shkumagai <take.this.2.your.grave@gmail.com>
2016-12-13 12:56:09 +0900

    update 53.erlang.el

--- conf/53-erlang.el
+++ conf/53-erlang.el
@@ -9,17 +9,17 @@
 (when (eq window-system 'x)
   (setq erlang-root-dir "/usr/lib64/erlang"))
 
-(defun get-emacs-ext-dir ()
-  (interactive)
-  (replace-regexp-in-string "\n+$"
-                            ""
-                            (shell-command-to-string
-                             (format "find %s -name \"emacs\" -type d -print"
-                                     erlang-root-dir))))
-
-;; $ find /usr/lib /usr/lib64 /opt/local/lib -type d -name "emacs"
-(add-to-list 'load-path (get-emacs-ext-dir))
-
 (when (boundp 'erlang-root-dir)
+  (defun get-emacs-ext-dir ()
+    (interactive)
+    (replace-regexp-in-string "\n+$"
+                              ""
+                              (shell-command-to-string
+                               (format "find %s -name \"emacs\" -type d -print"
+                                       erlang-root-dir))))
+
+  ;; $ find /usr/lib /usr/lib64 /opt/local/lib -type d -name "emacs"
+  (add-to-list 'load-path (get-emacs-ext-dir))
+
   (add-to-list 'exec-path (concat erlang-root-dir "/bin"))
   (use-package erlang-start))
