115416aa792d0fddb7ed71192facdc9b3d4425d8
shkumagai <take.this.2.your.grave@gmail.com>
2019-03-06 00:34:54 +0900

    Make setup.sh simple

--- setup.sh
+++ setup.sh
@@ -1,31 +1,4 @@
 #!/bin/bash
 
-### local functions
-
-function comment_out ()
-{
-    conf=${1}
-    sed -i.orig -e "s/\(^(load-library \".*-${1}\".*$\)/;; \1/g" init.el
-}
-
-function auto_config ()
-{
-    name=${1}
-    echo "name: ${name}"
-    if [ ! -x "/usr/local/bin/${name}" -a ! -x "/opt/local/bin/${name}" ]; then
-        comment_out ${1}
-    fi
-}
-
-### main
-
-## automatic configuration for init.el
-auto_config cmigemo
-auto_config ctags
-auto_config go
-
-## If already exists `~/.emacs.d' directory, remove it.
 [[ -d "$HOME/.emacs.d" ]] && rm -rf "$HOME/.emacs.d"
-
-## make symlink to source on user home.
 ln -Fis "$PWD" "$HOME/.emacs.d"
